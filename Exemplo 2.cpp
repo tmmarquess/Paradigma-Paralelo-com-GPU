efe55f7ec20854a6657de6e53a57187e198edc02
Thiago Marcelo Marques-TMARQUES <tmarques@motorola.com>
2022-12-21 14:44:13 -0400

    add example codes

---
+++ Exemplo 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+int main(void)
+{
+	int i;
+	int x;
+	x = 44;
+#pragma omp parallel for private(x)
+	for (i = 0; i <= 10; i++)
+	{
+		x = i;
+		printf("Thread number: %d x: %d\n", omp_get_thread_num(), x);
+	}
+	printf("x is %d\n", x);
+}
